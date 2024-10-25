0ef11231dc9036a836a09b55f55bcfe4dee4d2f3
Raj Thakur <raj.thakur@softude.com>
2024-10-25 18:46:08 +0530

    add Repository

---
+++ TestWeb.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TestWeb.DataAccess.Repository.IRepository;
+using TestWeb.DataAccess.Data;
+using TestWeb.DataAccess.Repository;
+using TestWeb.Models;
+
+namespace TestWeb.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
